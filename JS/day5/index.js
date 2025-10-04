bfdcdb0259bda5b1d3beac49ebab0e3dc67e2b18
Ankur Mittal <ankurmittl9081@gmail.com>
2025-10-04 20:27:42 +0530

    first commit

--- JS/day5/index.js
+++ JS/day5/index.js
@@ -1,11 +1,63 @@
 //Number
-let a=10;
-let b=345.14467584;
+// let a=10;
+// let b=345.14467584;
 // console.log(a);
 // console.log(typeof b.toFixed(2));//After decimal  only give two value
 // console.log(b);
 
-console.log(b.toPrecision(3));
+const { log } = require("console")
+
+// console.log(b.toPrecision(3));
+// console.log(b.toString());number to string
+
+
+// let a=new Number(20);
+// let b=new Number(20);
+// console.log(a==b);//false--> bcz it is a object 
+// let b=a;
+// console.log(a==b);//true
+
+// console.log(a);///type is object
+// console.log(b);
+
+// let obj1={
+//     name: "Ankur"
+// }
+// let obj2=obj1
+// console.log(obj1==obj2);//true
+
+// let obj2={
+//     name: "Ankur"
+// }
+// console.log(obj1==obj2);//false
+// console.log(obj1);
+// console.log(Boolean(new Number(0)));//true
+
+//Non primitive :Reference base pe compare
+//primitive copy by value
+
+// let a=10;
+// let b=a;
+
+// console.log(a==b);//true
+
+//Math
+// console.log(Math.abs(-4));
+// console.log(Math.PI.toPrecision(3));
+// console.log(Math.LN2.toFixed(3));
+// console.log(Math.SQRT2.toFixed(3));
+// console.log(Math.ceil(6.3));
+// console.log(Math.floor(6.3));
+// console.log(Math.log10(20));
+// console.log(Math.max(20,40,12,314));
+// console.log(Math.min(20,40,12,314));
+// imp
+// console.log(Math.random());//random value generate [0(incluxive)-1(exlcuxive)]
+
+//satte bhajhi ka game banana h:(0-9)
+console.log(Math.ceil(Math.random()*10));
+
+
 
 
 
