1296d6b59595b83879d6d2645c5ca273c5bd5c21
Ankur Mittal <ankurmittl9081@gmail.com>
2025-10-04 20:05:54 +0530

    first commit

---
+++ JS/day5/index.js
@@ -0,0 +1,12 @@
+//Number
+let a=10;
+let b=345.14467584;
+// console.log(a);
+// console.log(typeof b.toFixed(2));//After decimal  only give two value
+// console.log(b);
+
+console.log(b.toPrecision(3));
+
+
+
+
